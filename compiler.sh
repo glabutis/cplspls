e28f1284dd1c2be6fa520a2da5db93191812f849
Grant Labutis <46079274+glabutis@users.noreply.github.com>
2019-12-02 22:21:19 -0500

    Updated compiler 12.2.19

--- compiler.sh
+++ compiler.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-path=".etc/textedit.txt"
-while IFS= read -r line
-do 
-liney = "$line"
-done < "$input"
 cd .projects
+green = '\033[0;32m'
+nc = '\033[0m'
 function new() {
 read -p "Enter the project name below: " name
 mkdir ".$name"
@@ -42,20 +39,28 @@ function show() {
 	startScreen	
 }
 function startScreen() {
-echo "   _____       _           _  "
-echo "  / ____|     | |         | |    "
-echo " | |     _ __ | |___ _ __ | |___ "
-echo " | |    | '_ \| / __| '_ \| / __|"
-echo " | |____| |_) | \__ \ |_) | \__ \\"
-echo "  \_____| .__/|_|___/ .__/|_|___/"
-echo "        | |         | |          "
-echo "        |_|         |_|          "
-echo "Select below which option you would like to use..."
-echo "1: Make new project"
-echo "2. Open project"
-echo "3. Run project"
-echo "4. Show projects"
+clear
+echo -e "\e[0;31m   _____       _           _  "
+echo -e "  / ____|     | |         | |    "
+echo -e " | |     _ __ | |___ _ __ | |___ "
+echo -e " | |    | '_ \| / __| '_ \| / __|"
+echo -e " | |____| |_) | \__ \ |_) | \__ \\"
+echo -e "  \_____| .__/|_|___/ .__/|_|___/"
+echo -e "        | |         | |          "
+echo -e "        |_|         |_|         \e[0m "
+echo -e "Select below which option you would like to use..."
+echo -e "1: \e[0;32mMake new project \e[0m"
+echo -e "2. \e[0;32mOpen project \e[0m"
+echo -e "3. \e[0;32mRun project \e[0m"
+echo -e "4. \e[0;32mShow projects \e[0m"
 read -p ">>" option
+if [ $option == 0 ] 
+then
+clear 
+echo " Exiting now........"
+exit
+fi
+
 if [ $option == 1 ]
 then
 new
@@ -75,6 +80,15 @@ if [ $option == 4 ]
 then 
 show
 fi
+
+if [ $option != 1 ] && [ $option != 2 ] && [ $option != 3 ] && [ $option != 4 ] && [ $option != 0 ] 
+then 
+echo "Not a valid command"
+read -p "Press enter to continue..."
+startScreen
+fi
+
 }
+
 
 startScreen
